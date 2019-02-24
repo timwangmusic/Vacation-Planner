4770794e636c0d850a3251aee49a525135cb4045
xtpitt <xuantian19880401@gmail.com>
2019-02-23 18:04:20 -0800

    added weight types and functions

--- graph/weight.go
+++ graph/weight.go
@@ -16,7 +16,6 @@ func (this SimpleWeight) GetWeight(weight uint32) uint32{
 
 func (l SimpleWeight) Compare(r SimpleWeight) bool {
 	if l.w <= r.w{
-
 		return true
 	} else {
 		return false
@@ -24,11 +23,6 @@ func (l SimpleWeight) Compare(r SimpleWeight) bool {
 }
 
 
-type SimpleBaseWeight struct {
-	timeInMin uint32
-	budget	float64
-	cmpflag	uint8
-}
 func (this SimpleBaseWeight) SetWeight(timeInMin uint32, budget float64){
 	this.timeInMin = timeInMin
 	this.budget = budget}
@@ -57,7 +51,14 @@ of the object calling the compare function. This configuration must
 be used with care.
  */
 
-
+type SimpleBaseWeight struct{
+	timeInMin uint32
+	budget    float64
+	/*
+	Need to solve cmpflag match problems, make sure cmpflag matches before calling the comparison function
+	 */
+	cmpflag uint8
+}
 func (v SimpleBaseWeight) Setcmpflag(cmpflag uint8){
 	/*
 	Need to perform validity check of input values
@@ -68,12 +69,15 @@ func (v SimpleBaseWeight) Setcmpflag(cmpflag uint8){
 func (v SimpleBaseWeight) Getcmpflag() uint8{
 	return v.cmpflag
 }
-
+/*
+FIXME: The priority of configuration is based only on the cmpflag
+of the object calling the compare function. This configuration must
+be used with care.
+ */
 func (l SimpleBaseWeight) Compare(r SimpleBaseWeight) bool{
 	switch l.cmpflag {
 	case PRIORITY_TIME :
 		if(l.timeInMin <=r.timeInMin){
-
 			return true
 		} else {
 			return false
