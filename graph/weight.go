2fd6795e2fec58b071aa86c0e46e02cdb5164d65
xtpitt <xuantian19880401@gmail.com>
2019-03-02 21:36:19 -0800

    Solved conflict between weight.go and main.go

--- graph/weight.go
+++ graph/weight.go
@@ -1,4 +1,5 @@
 package graph
+<<<<<<< Updated upstream
 const(
 	PRIORITY_TIME=iota
 	PRIORITY_BUDGET
@@ -14,11 +15,33 @@ func (this SimpleWeight) GetWeight(weight uint32) uint32{
 }
 func (l SimpleWeight) Compare(r SimpleWeight) bool{
 	if(l.w <= r.w){
+=======
+/*
+This source provides the polymorphism like compare functions for different kind of weights in the graph
+ */
+ const(
+ 	PRIORITY_TIME=iota
+ 	PRIORITY_BUDGET
+ )
+type SimpleWeight struct{
+	w uint64
+}
+/*
+ type Comparable interface{
+ 	Compare(r Comparable) bool
+ }
+ */
+
+
+func (l SimpleWeight) Compare(r SimpleWeight) bool {
+	if l.w <= r.w{
+>>>>>>> Stashed changes
 		return true
 	} else {
 		return false
 	}
 }
+<<<<<<< Updated upstream
 
 type SimpleBaseWeight struct {
 	timeInMin uint32
@@ -56,25 +79,78 @@ func (l SimpleBaseWeight) Compare(r SimpleBaseWeight) bool {
 	switch l.cmpflag {
 	case PRIORITY_TIME:
 		if l.timeInMin <= r.timeInMin {
+=======
+func (v SimpleWeight) SetWeight(weight uint64){
+	v.w = weight
+}
+func (v SimpleWeight) GetWeight(weight uint64) uint64{
+	return v.w;
+}
+
+type SimpleBaseWeight struct{
+	timeInMin uint32
+	budget    float64
+	/*
+	Need to solve cmpflag match problems, make sure cmpflag matches before calling the comparison function
+	 */
+	cmpflag uint8
+}
+func (v SimpleBaseWeight) SetWeight(timeInMin uint32, budget float64){
+	v.timeInMin = timeInMin
+	v.budget = budget
+}
+func (v SimpleBaseWeight) GetWeight() (uint32, float64){
+	return v.timeInMin, v.budget
+}
+func (v SimpleBaseWeight) Setcmpflag(cmpflag uint8){
+	/*
+	Need to perform validity check of input values
+	will change function signiture to bool after then
+	 */
+	v.cmpflag = cmpflag
+}
+func (v SimpleBaseWeight) Getcmpflag() uint8{
+	return v.cmpflag
+}
+
+func (l SimpleBaseWeight) Compare(r SimpleBaseWeight) bool{
+	switch l.cmpflag {
+	case PRIORITY_TIME :
+		if(l.timeInMin <=r.timeInMin){
+>>>>>>> Stashed changes
 			return true
 		} else {
 			return false
 		}
+<<<<<<< Updated upstream
 	case PRIORITY_BUDGET:
 		if l.budget <= r.budget {
+=======
+	case PRIORITY_BUDGET :
+		if(l.budget<=r.budget){
+>>>>>>> Stashed changes
 			return true
 		} else {
 			return false
 		}
 	default:
+<<<<<<< Updated upstream
 		/*
 		Default behavior favor money
 		 */
 		if l.budget <= r.budget {
+=======
+		if(l.budget<=r.budget){
+>>>>>>> Stashed changes
 			return true
 		} else {
 			return false
 		}
 	}
+<<<<<<< Updated upstream
 }
 
+=======
+	
+}
+>>>>>>> Stashed changes
--- main/main.go
+++ main/main.go
@@ -72,6 +72,42 @@ func main() {
 	//for _, place := range places{
 	//	fmt.Println(place.GetName())
 	//}
+
+	//pitt := graph.Vertex{Location:Pittsburg, Name:"Pitt"}
+	//sd := graph.Vertex{Location:SanDiego, Name:"SD"}
+	//vertexes := []*graph.Vertex{&pitt, &sd}
+	//testTreeTraversal(&sd, vertexes)
+
+
+	//Test MX
+
+	sw1:=graph.SimpleWeight{}
+	sw2:=graph.SimpleWeight{}
+	sw1.SetWeight(5)
+	sw2.SetWeight(6)
+	if sw1.Compare(sw2){
+		fmt.Println("Simple: Left is NMT Right.")
+	} else {
+		fmt.Print("Simple: Right is NMR Left")
+	}
+	//FIXME: graph function mismatch, version control problems
+	sbw1 := graph.SimpleBaseWeight{}
+	sbw2 := graph.SimpleBaseWeight{}
+	sbw1.SetWeight(50, 100.25)
+	sbw2.SetWeight(60, 99)
+	sbw1.Setcmpflag(graph.PRIORITY_TIME)
+	sbw2.Setcmpflag(graph.PRIORITY_BUDGET)
+	if sw1.Compare(sw2){
+		fmt.Println("Base_Time: Left is NMT Right.")
+	} else {
+		fmt.Print("Base_Time: Right is NMR Left")
+	}
+
+	if sw2.Compare(sw1){
+		fmt.Println("Base_Budget: Right is NMT Left.")
+	} else {
+		fmt.Print("Base_Budget: Left is NMR Right")
+	}
 }
 
 func testPriorityQueueInterface(pq graph.PriorityQueue, nodes []*graph.Vertex){
