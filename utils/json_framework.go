4f883789070eaaba27f15e35fcfa69c2c0c87ba9
xtpitt <xuantian19880401@gmail.com>
2019-03-02 18:59:49 -0800

    implemented universal json parsing function in utils/

--- utils/json_framework.go
+++ utils/json_framework.go
@@ -1,11 +1,52 @@
-package test
+package utils
 
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+)
 
-type jsonParse interface{
-	readFromFile(dir string, placeptr *interface{})
+/*
+Base interface, can be extended to arbitrary structs/Json
+Objects.
+ */
+
+type Place struct{
+	/*FIXME: Need to set private variables and did
+	tedious set/get functions?
+	*/
+	Location string `json:location`
+	Name string `json:name`
+	Addr string	`json: addr`
+	LocationType string `json:locationtype`
+	PlaceId	string `json:placeid`
+	PriceLevel int	`json:pricelevel`
 }
 
+func ReadFromFile(fname string, ptr interface{}) error{
+	if fname == "" {
+		/*FIXME: Need directory management fucntions*/
+		fname = "create_visit_location_test_001.json"
+	}
+	jsonFile, err := os.Open(fname)
+	if err != nil {
+		/*FIXME: Need to be integrated to native log functions*/
+		fmt.Println(err)
+		return err
+	}
+	/*FIXME: How to handle this error*/
+	defer jsonFile.Close()
 
+	byteValue, _ := ioutil.ReadAll(jsonFile)
+	err = json.Unmarshal(byteValue, ptr)
+	if err != nil {
+		/*FIXME: Need to be integrated to native log functions*/
+		fmt.Println(err)
+		return err
+	}
+	return nil
+}
 
 
 
