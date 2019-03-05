428c32feafd2f40fce03f3ade1650745ded8739a
Weihe <weihewang2012@gmail.com>
2019-03-04 23:39:44 -0800

    add function to match cluster centers so that the sum distance of all centers is minimized

--- utils/math.go
+++ utils/math.go
@@ -52,3 +52,24 @@ func FindCenter(points [][]float64) ([]float64, error){
 	centralLat := math.Atan2(z, math.Sqrt(x * x + y * y)) * 180 / math.Pi
 	return []float64{centralLat, centralLng}, nil
 }
+
+// do not consider duplication in input number list
+func Permutations(nums []int, res *[][]int, start int){
+	if start == len(nums){
+		tmp := make([]int, len(nums))
+		copy(tmp, nums)
+		*res = append(*res, tmp)
+		return
+	}
+	for i:=start; i < len(nums); i++{
+		swap(&nums, start, i)
+		Permutations(nums, res, start+1)
+		swap(&nums, i, start)
+	}
+}
+
+func swap(nums *[]int, i int, j int){
+	tmp := (*nums)[i]
+	(*nums)[i] = (*nums)[j]
+	(*nums)[j] = tmp
+}
