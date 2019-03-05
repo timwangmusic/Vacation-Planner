a1ff1baf052c5b53cac599151f422d126736147d
Weihe <weihewang2012@gmail.com>
2019-03-04 23:39:44 -0800

    add function to match cluster centers so that the sum distance of all centers is minimized

---
+++ matching/match_cluster_centers.go
@@ -0,0 +1,56 @@
+package matching
+
+import (
+	"Vacation-planner/utils"
+	"errors"
+	"math"
+)
+
+type clusterCenterPair struct{
+	EateryIdx int
+	VisitIdx  int
+}
+
+func MatchClusterCenters(eClusterCenters [][]float64, vClusterCenters [][]float64) (pairs []clusterCenterPair, err error){
+	if len(eClusterCenters) != len(vClusterCenters){
+		return []clusterCenterPair{}, errors.New("number of eatery clusters and visit clusters must be the same")
+	}
+
+	numClusters := len(eClusterCenters)
+	indexes := make([]int, numClusters)
+	for i:=0; i<numClusters; i++{
+		indexes[i] = i
+	}
+
+	permutations := [][]int{}
+	utils.Permutations(indexes, &permutations, 0)
+
+	score := math.Inf(1)	// positive infinity
+
+	finalPermutation := []int{}
+	for _, permutation := range permutations{
+		pScore := calculateScore(eClusterCenters, vClusterCenters, permutation)
+		if pScore < score{
+			score = pScore
+			finalPermutation = permutation
+		}
+	}
+
+	for k, index := range finalPermutation{
+		pairs = append(pairs, clusterCenterPair{k, index})
+	}
+
+	return
+}
+
+func calculateScore(eClusterCenters [][]float64, vClusterCenters [][]float64, permutation []int) float64{
+	score := 0.0
+	for k, index := range permutation{
+		score += clusterDistance(eClusterCenters[k], vClusterCenters[index])
+	}
+	return score
+}
+
+func clusterDistance(clusterA []float64, clusterB []float64) float64{
+	return utils.HaversineDist(clusterA, clusterB)
+}
---
+++ test/match_cluster_centers_test.go
@@ -0,0 +1,29 @@
+package test
+
+import (
+	"Vacation-planner/matching"
+	"testing"
+)
+
+func TestMatchClusterCenters (t *testing.T){
+	eateryCenters := [][]float64{
+		{37.773972, -122.431297},
+		{32.715736, -117.161087},
+		{40.712776, -74.005974},
+	}
+	visitCenters := [][]float64{
+		{34.052235, -118.243683},
+		{36.169941, -115.139832},
+		{40.779079, -73.962578},
+	}
+
+	clusterPairs, _ := matching.MatchClusterCenters(eateryCenters, visitCenters)
+
+	expected := [][]int{{0, 1}, {1, 0}, {2, 2}}
+
+	for k, pair := range clusterPairs{
+		if pair.EateryIdx != expected[k][0] || pair.VisitIdx != expected[k][1]{
+			t.Errorf("Incorrect cluster matching result. Expected %T, got %T", expected[k], pair)
+		}
+	}
+}
