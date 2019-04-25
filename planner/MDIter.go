6fc8e5908136ae34b00577eb83eebda54d7bd014
xtpitt <xuantian19880401@gmail.com>
2019-04-24 23:43:24 -0700

    adjusted MDIter to planner package and some relavent refactor
    adjusted MDIter to planner package and some relavent refactor
    
    adjusted MDIter to planner package and some relavent refactor

---
+++ planner/MDIter.go
@@ -0,0 +1,60 @@
+package planner
+
+type MDtagIter struct {
+	Tag    string
+	Status []int
+	Size   []int
+}
+
+func (this *MDtagIter) Init(tag string, sizeE int, sizeV int) bool {
+	if tag == "" {
+		return false
+	}
+	this.Tag = tag
+	this.Status = make([]int, len(tag))
+	this.Size = make([]int, len(tag))
+	for pos, char := range tag {
+		this.Status[pos] = 0
+		if char =='E' || char == 'e' {
+			this.Size[pos] =  sizeE
+		} else if char =='V' || char == 'v'{
+			this.Size[pos] = sizeV
+		}
+		if this.Size[pos] == 0 {
+			return false
+		}
+	}
+	return true
+}
+func (iterator *MDtagIter) HasNext() bool {
+	for i, _ := range iterator.Tag {
+		if iterator.Status[i] < iterator.Size[i] - 1 {
+			return true
+		}
+	}
+	return false
+}
+
+func (this *MDtagIter) Next() bool{
+	l := len(this.Tag)
+	return this.plusone(l-1)
+}
+func (this *MDtagIter) plusone(l int) bool {
+	if l < 0 {
+		//log fault
+		return false
+	}
+	if this.Status[l] + 1 == this.Size[l] {
+		this.Status[l] = 0
+		return this.plusone(l-1)
+	} else {
+		this.Status[l] += 1
+		return true
+	}
+}
+
+func (this *MDtagIter) Reset()  {
+	for i := range this.Tag {
+		this.Status[i] = 0
+	}
+}
