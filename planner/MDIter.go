1b8e91cb23ec0b3606b8f5d8e1bd9d4eaa7de40a
weihewang <weihewang@didiglobal.com>
2019-06-05 16:48:38 -0700

    reformat planner package with gofmt tool

--- planner/MDIter.go
+++ planner/MDIter.go
@@ -17,9 +17,9 @@ func (this *MDtagIter) Init(tag string, cplaces CategorizedPlaces) bool {
 	this.Size = make([]int, len(tag))
 	for pos, char := range tag {
 		this.Status[pos] = 0
-		if char =='E' || char == 'e' {
-			this.Size[pos] =  sizeE
-		} else if char =='V' || char == 'v'{
+		if char == 'E' || char == 'e' {
+			this.Size[pos] = sizeE
+		} else if char == 'V' || char == 'v' {
 			this.Size[pos] = sizeV
 		}
 		if this.Size[pos] == 0 {
@@ -30,32 +30,32 @@ func (this *MDtagIter) Init(tag string, cplaces CategorizedPlaces) bool {
 }
 func (iterator *MDtagIter) HasNext() bool {
 	for i, _ := range iterator.Tag {
-		if iterator.Status[i] < iterator.Size[i] - 1 {
+		if iterator.Status[i] < iterator.Size[i]-1 {
 			return true
 		}
 	}
 	return false
 }
 
-func (this *MDtagIter) Next() bool{
+func (this *MDtagIter) Next() bool {
 	l := len(this.Tag)
-	return this.plusone(l-1)
+	return this.plusone(l - 1)
 }
 func (this *MDtagIter) plusone(l int) bool {
 	if l < 0 {
 		//log fault
 		return false
 	}
-	if this.Status[l] + 1 == this.Size[l] {
+	if this.Status[l]+1 == this.Size[l] {
 		this.Status[l] = 0
-		return this.plusone(l-1)
+		return this.plusone(l - 1)
 	} else {
 		this.Status[l] += 1
 		return true
 	}
 }
 
-func (this *MDtagIter) Reset()  {
+func (this *MDtagIter) Reset() {
 	for i := range this.Tag {
 		this.Status[i] = 0
 	}
--- planner/categorization.go
+++ planner/categorization.go
@@ -2,21 +2,21 @@ package planner
 
 import "Vacation-planner/matching"
 
-type CategorizedPlaces struct{
+type CategorizedPlaces struct {
 	EateryPlaces []matching.Place
-	VisitPlaces []matching.Place
+	VisitPlaces  []matching.Place
 }
 
-func Categorize(cluster *matching.PlaceCluster) CategorizedPlaces{
+func Categorize(cluster *matching.PlaceCluster) CategorizedPlaces {
 	res := CategorizedPlaces{
 		EateryPlaces: make([]matching.Place, 0),
-		VisitPlaces: make([]matching.Place, 0),
+		VisitPlaces:  make([]matching.Place, 0),
 	}
 
-	for _, place := range cluster.Places{
-		if place.CatTag == "visit"{
+	for _, place := range cluster.Places {
+		if place.CatTag == "visit" {
 			res.VisitPlaces = append(res.VisitPlaces, place)
-		} else if place.CatTag == "eatery"{
+		} else if place.CatTag == "eatery" {
 			res.EateryPlaces = append(res.EateryPlaces, place)
 		}
 	}
