2305d3275da42a2ea6549ced30413f461db80825
weihewang <weihewang@didiglobal.com>
2019-07-25 09:53:39 -0700

    simplify set/get places for redis client

--- iowrappers/redis_client.go
+++ iowrappers/redis_client.go
@@ -3,6 +3,7 @@ package iowrappers
 import (
 	"Vacation-planner/POI"
 	"Vacation-planner/utils"
+	"encoding/json"
 	"fmt"
 	"github.com/go-redis/redis"
 	"strconv"
@@ -22,23 +23,12 @@ func (redisClient *RedisClient) Init(addr string, password string, databaseIdx i
 	})
 }
 
-// Store a single place as a hash in Redis with key as the place ID
-func (redisClient *RedisClient) storePlace(place POI.Place) {
-	placeId := place.ID
-	placeName := place.Name
-	locationType := place.LocationType
-	formattedAddr := place.FormattedAddress
-	priceLevel := place.PriceLevel
-	rating := place.Rating
-	hours := strings.Join(place.Hours[:], ";")
+// serialize place using JSON and store in Redis with key as the place ID
+func (redisClient *RedisClient) setPlace(place POI.Place) {
+	json_, err := json.Marshal(place)
+	utils.CheckErr(err)
 
-	redisClient.client.HSet(placeId, "placeId", placeId)
-	redisClient.client.HSet(placeId, "placeName", placeName)
-	redisClient.client.HSet(placeId, "locationType", locationType)
-	redisClient.client.HSet(placeId, "address", formattedAddr)
-	redisClient.client.HSet(placeId, "priceLevel", priceLevel)
-	redisClient.client.HSet(placeId, "rating", rating)
-	redisClient.client.HSet(placeId, "hours", hours)
+	redisClient.client.Set(place.ID, json_, -1)
 }
 
 // Store places obtained from database in Redis
@@ -53,33 +43,15 @@ func (redisClient *RedisClient) StorePlacesForLocation(location string, places [
 	for _, place := range places {
 		dist := utils.HaversineDist([]float64{lng, lat}, place.Location.Coordinates[:])
 		client.ZAdd(location, redis.Z{dist, place.ID})
-		redisClient.storePlace(place)
+		redisClient.setPlace(place)
 	}
 }
 
 func (redisClient *RedisClient) getPlace(placeId string) (place POI.Place) {
-	values, err := redisClient.client.HGetAll(placeId).Result()
+	res, err := redisClient.client.Get(placeId).Result()
 	utils.CheckErr(err)
 
-	place.ID = values["placeId"]
-	place.Name = values["placeName"]
-	place.LocationType = values["locationType"]
-	place.FormattedAddress = values["address"]
-
-	price, err := strconv.ParseInt(values["priceLevel"], 10, 64)
-	utils.CheckErr(err)
-	place.PriceLevel = int(price)
-
-	rating, err := strconv.ParseFloat(values["rating"], 64)
-	utils.CheckErr(err)
-	place.Rating = float32(rating)
-
-	hours := strings.Split(values["hours"], ";")
-
-	for idx, hour := range hours {
-		place.Hours[idx] = hour
-	}
-
+	utils.CheckErr(json.Unmarshal([]byte(res), &place))
 	return
 }
 
