8884444114fc109423eda5b24258689ece6716bc
Weihe <weihewang2012@gmail.com>
2019-08-04 15:18:28 -0700

    fix two test cases

--- test/solution_candidate_selection_test.go
+++ test/solution_candidate_selection_test.go
@@ -7,15 +7,15 @@ import (
 
 // test if priority queue gives top solution candidates
 func TestSolutionCandidateSelection (t *testing.T){
-	candidates := make([]solution.SolutionCandidate, 0)
+	candidates := make([]solution.SlotSolutionCandidate, 0)
 
 	for i := 0; i < 100; i++{
-		candidates = append(candidates, solution.SolutionCandidate{Score: float64(i)})
+		candidates = append(candidates, solution.SlotSolutionCandidate{Score: float64(i)})
 	}
 
 	res := solution.FindBestCandidates(candidates)
 
-	expectation := []float64{95, 96, 97, 98, 99}
+	expectation := []float64{85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99}
 	for idx, r := range res{
 		if r.Score != expectation[idx]{
 			t.Errorf("expected %f, got %f", expectation[idx], r.Score)
