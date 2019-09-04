d559b8d93eabcc4ecd67e391b8787bb2472bd71d
Xiao <xuantian19880401@gmail.com>
2019-09-03 20:51:47 -0700

    Added missed changes

--- POI/time_intervals.go
+++ POI/time_intervals.go
@@ -99,7 +99,7 @@ func ParseTimeInterval(openingHour string) (interval TimeInterval, err error) {
 func calculateHour(time string, am_pm string) uint8 {
 	t := strings.Split(time, ":")
 	hour, err := strconv.ParseUint(t[0], 10, 8)
-	utils.CheckErr(err)
+	utils.CheckErrImmediate(err, utils.LogError)
 
 	if am_pm == "AM" || am_pm == "am" {
 		return uint8(hour)
