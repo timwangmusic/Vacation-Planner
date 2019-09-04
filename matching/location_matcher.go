e0d6496e56ad6a31d5f1b9c9d4e829918db7d1bb
Xiao <xuantian19880401@gmail.com>
2019-09-03 20:51:47 -0700

    Added missed changes

--- matching/location_matcher.go
+++ matching/location_matcher.go
@@ -41,7 +41,7 @@ func (m *LocationMatcher) Matching(req *LocationMatchingRequest, mapsClient *iow
 	// pair cluster centers from two categories
 	clusterPairs, err := MatchClusterCenters(*m.VisitClusterMgr.ClusterCenters, *m.EateryClusterMgr.ClusterCenters)
 
-	utils.CheckErr(err)
+	utils.CheckErrImmediate(err, utils.LogError)
 
 	for _, clusterPair := range clusterPairs{
 		placePairs = append(placePairs, m.genPlacePairs(&clusterPair)...)
