0988b3e25bfbe2095c29a1f9b62f1b4f627923d1
sananand007 <anandjairam@ufl.edu>
2020-01-07 18:11:04 -0800

    Front end: Added bootstrap components for Form

--- planner-app/src/components/Login.js
+++ planner-app/src/components/Login.js
@@ -1,52 +1,45 @@
-import React, { useState } from "react"
+import React from "react"
 import Form from 'react-bootstrap/Form'
-import { Button, FormGroup, FormControl, FormLabel } from "react-bootstrap"
-import "./Login.css"
+import Button from 'react-bootstrap/Button'
+import Col from 'react-bootstrap/Col'
 
+const Login = () => {
+  return (
+<Form>
+    <Form.Row>
+        <Form.Group as={Col} controlId="formGridFirstName">
+            <Form.Label>First Name</Form.Label>
+            <Form.Control type='first name' placeholder="First name" />
+        </Form.Group>
 
-function Login(props) {
-	const [email, setEmail] = useState("");
-	const [password, setPassword] = useState("");
+        <Form.Group as={Col} controlId="formGridSecondName">
+            <Form.Label>Last Name</Form.Label>
+            <Form.Control type='last name' placeholder="Last name"/>
+        </Form.Group>
+    </Form.Row>
 
-	function validateForm() {
-		return email.length > 0 && password.length > 0;
-	}
+    <Form.Group controlId="formBasicEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control type="email" placeholder="Enter email" />
+        <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+        </Form.Text>
+  </Form.Group>
 
-	function handleSubmit(event) {
-		event.preventDefault();
-	}
+  <Form.Group controlId="formBasicPassword">
+    <Form.Label>Password</Form.Label>
+    <Form.Control type="password" placeholder="Password" />
+    </Form.Group>
+        <Form.Group controlId="formBasicCheckbox">
+            <Form.Check type="checkbox" label="Check me out" />
+    </Form.Group>
 
-	return (
-	<div className="Login">
-        <div class="modal-body">
-            <form class="form-horizontal" role="form" onSubmit={handleSubmit}>
-	            <div class="row">
-                <div class="form-group">
-                <label class="control-label col-xs-2">Title:</label>
-                <div class="col-xs-10">
-                    <input  type="text" class="form-control" data-bind="value: title" />
-                </div>
-                </div>
-                </div>
-        <div class="row">
-            <div class="form-group">
-                <label class="control-label col-xs-2">Start:</label>
-                <div class="col-xs-4">
-                   <div class="input-group">
-                      <input  type="text" class="form-control" data-bind="value: title" />
-                      <span class="input-group-addon"><i class="fa fa-calendar"></i></span>
-                </div>
-               </div>
-            </div>
-        </div>
-  <Button variant="primary" type="submit">
-    Submit
-  </Button> 
-            </form>
-        </div>
-        </div>
-	);
+    <Button variant="primary" type="submit">
+        Submit
+    </Button>
+
+</Form>
+  );
 }
-
 
 export default Login
