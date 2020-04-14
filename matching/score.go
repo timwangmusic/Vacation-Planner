3d9627e06ada5940c276e2843a829a9264784c67
Xiao <xuantian19880401@gmail.com>
2020-04-14 16:03:13 -0700

    initalize v2 record table with getKey api

--- matching/score.go
+++ matching/score.go
@@ -17,34 +17,32 @@ const (
 
 const SelectionThreshold = -1
 
-
-
-type KnapsackRecordTable struct{
-	timeLimit uint8
-	budget uint
+type KnapsackRecordTable struct {
+	timeLimit   uint8
+	budget      uint
 	SavedRecord map[uint]KnapsackNodeRecord
-	NewRecord map[uint]KnapsackNodeRecord
+	NewRecord   map[uint]KnapsackNodeRecord
 }
 
-func (this *KnapsackRecordTable) Init(timeLimit uint8, budget uint){
+func (this *KnapsackRecordTable) Init(timeLimit uint8, budget uint) {
 	this.timeLimit = timeLimit
 	this.budget = budget
 	this.SavedRecord = make(map[uint]KnapsackNodeRecord)
 	this.NewRecord = make(map[uint]KnapsackNodeRecord)
-	start := KnapsackNodeRecord{0,0,SelectionThreshold, make([]Place, 0)}
-	this.SavedRecord[0] = start
+	start := KnapsackNodeRecord{0, 0, SelectionThreshold, make([]Place, 0)}
+	this.SavedRecord[this.getKey(0, 0)] = start
 }
 func (this *KnapsackRecordTable) getKey(timeLimit uint8, budget uint) (key uint) {
-	key = uint(timeLimit)*this.budget+budget
+	key = uint(timeLimit)*this.budget + budget
 	return
 }
-func (this *KnapsackRecordTable) getTimeLimitAndCost(key uint) (timeLimt uint8, budget uint){
+func (this *KnapsackRecordTable) getTimeLimitAndCost(key uint) (timeLimt uint8, budget uint) {
 	budget = key % this.budget
-	timeLimt = uint8((key - budget)/this.budget)
+	timeLimt = uint8((key - budget) / this.budget)
 	return
 }
 func (this *KnapsackRecordTable) update() {
-	for key, record := range(this.NewRecord){
+	for key, record := range this.NewRecord {
 		if oldRecord, ok := this.SavedRecord[key]; ok {
 			if oldRecord.score < record.score {
 				this.SavedRecord[key] = record
@@ -65,52 +63,53 @@ type KnapsackNodeRecord struct {
 }
 
 type KnapsackNode struct {
-	score float64
+	score    float64
 	solution []Place
 }
-func KnapsackMatrixCopy(dst [][]KnapsackNode, src [][]KnapsackNode){
-	rowDst:=len(dst)
-	rowSrc:=len(src)
-	if rowDst != rowSrc || rowDst ==0 || rowSrc == 0 {
+
+func KnapsackMatrixCopy(dst [][]KnapsackNode, src [][]KnapsackNode) {
+	rowDst := len(dst)
+	rowSrc := len(src)
+	if rowDst != rowSrc || rowDst == 0 || rowSrc == 0 {
 		return
 	}
-	colDst:=len(dst[0])
-	colSrc:=len(src[0])
+	colDst := len(dst[0])
+	colSrc := len(src[0])
 	if colDst != colSrc {
 		return
 	}
-	for i:=0; i<rowSrc; i++{
-		for j:=0; j<colSrc; j++{
+	for i := 0; i < rowSrc; i++ {
+		for j := 0; j < colSrc; j++ {
 			dst[i][j].score = src[i][j].score
 			dst[i][j].solution = append(make([]Place, 0, len(src[i][j].solution)), src[i][j].solution...)
 		}
 	}
 }
-func Knapsack(places []Place, timeLimit uint8, budget uint) (results []Place){
+func Knapsack(places []Place, timeLimit uint8, budget uint) (results []Place) {
 	//INIT
 	current := make([][]KnapsackNode, timeLimit+1)
-	for i:=0; i<int(timeLimit)+1; i++ {
+	for i := 0; i < int(timeLimit)+1; i++ {
 		current[i] = make([]KnapsackNode, budget+1)
-		for j:=0; j<int(budget)+1; j++{
+		for j := 0; j < int(budget)+1; j++ {
 			current[i][j].score = SelectionThreshold
 		}
 	}
 	next := make([][]KnapsackNode, timeLimit+1)
-	for i:=0; i<int(timeLimit)+1; i++ {
+	for i := 0; i < int(timeLimit)+1; i++ {
 		next[i] = make([]KnapsackNode, budget+1)
-		for j:=0; j<int(budget)+1; j++{
+		for j := 0; j < int(budget)+1; j++ {
 			next[i][j].score = SelectionThreshold
 		}
 	}
-	optimalNode := KnapsackNode{SelectionThreshold,make([]Place,0)}
+	optimalNode := KnapsackNode{SelectionThreshold, make([]Place, 0)}
 	tempPlaces := make([]Place, 0)
 	var tempScore float64 = SelectionThreshold
 	tempi := 0
 	tempj := 0
 	//MAIN
 	var staytime POI.StayingTime
-	for k:=0;k<len(places);k++{
-		KnapsackMatrixCopy(current,next)
+	for k := 0; k < len(places); k++ {
+		KnapsackMatrixCopy(current, next)
 		staytime = POI.GetStayingTimeForLocationType(places[k].PlaceType)
 		//Do 0,0
 		if uint8(staytime) <= timeLimit && int(math.Ceil(places[k].Price)) <= int(budget) {
@@ -118,18 +117,18 @@ func Knapsack(places []Place, timeLimit uint8, budget uint) (results []Place){
 			tempScore = Score(tempPlaces)
 			if tempScore > next[staytime][int(math.Ceil(places[k].Price))].score {
 				next[staytime][int(math.Round(places[k].Price))].score = tempScore
-				next[staytime][int(math.Round(places[k].Price))].solution =append(make([]Place, 0, len(tempPlaces)), tempPlaces...)
+				next[staytime][int(math.Round(places[k].Price))].solution = append(make([]Place, 0, len(tempPlaces)), tempPlaces...)
 			}
-			if tempScore > optimalNode.score{
+			if tempScore > optimalNode.score {
 				optimalNode.score = tempScore
 				optimalNode.solution = append(make([]Place, 0, len(tempPlaces)), tempPlaces...)
 			}
 		}
 		//Do others
-		for i:=0; i<int(timeLimit); i++ {
-			for j:=0; j<int(budget); j++{
-				if current[i][j].score > SelectionThreshold{
-					if i + int(staytime) <= int(timeLimit) && j + int(math.Ceil(places[k].Price)) <= int(budget){
+		for i := 0; i < int(timeLimit); i++ {
+			for j := 0; j < int(budget); j++ {
+				if current[i][j].score > SelectionThreshold {
+					if i+int(staytime) <= int(timeLimit) && j+int(math.Ceil(places[k].Price)) <= int(budget) {
 						tempi = i + int(staytime)
 						tempj = j + int(math.Ceil(places[k].Price))
 						tempPlaces = append(current[i][j].solution, places[k])
@@ -138,7 +137,7 @@ func Knapsack(places []Place, timeLimit uint8, budget uint) (results []Place){
 							next[tempi][tempj].score = tempScore
 							next[tempi][tempj].solution = append(make([]Place, 0, len(tempPlaces)), tempPlaces...)
 						}
-						if tempScore > optimalNode.score{
+						if tempScore > optimalNode.score {
 							optimalNode.score = tempScore
 							optimalNode.solution = append(make([]Place, 0, len(tempPlaces)), tempPlaces...)
 						}
@@ -150,18 +149,19 @@ func Knapsack(places []Place, timeLimit uint8, budget uint) (results []Place){
 	log.Debugf("Optimal rate %f", optimalNode.score)
 	return optimalNode.solution
 }
+
 /*
 Knapsack v2 uses sparse matrix like storage for step values and saves memory
- */
-func Knapsackv2(places []Place, timeLimit uint8, budget uint) (results []Place){
+*/
+func Knapsackv2(places []Place, timeLimit uint8, budget uint) (results []Place) {
 	//INIT
 	var recordtable KnapsackRecordTable
 	rt := &recordtable
 	rt.Init(timeLimit, budget)
-	optimalNode := KnapsackNodeRecord{0,0, SelectionThreshold,make([]Place,0)}
+	optimalNode := KnapsackNodeRecord{0, 0, SelectionThreshold, make([]Place, 0)}
 	//MAIN
 	var staytime POI.StayingTime
-	for k:=0;k<len(places);k++ {
+	for k := 0; k < len(places); k++ {
 		rt.update()
 		staytime = POI.GetStayingTimeForLocationType(places[k].PlaceType)
 		for key, record := range rt.SavedRecord {
@@ -193,10 +193,9 @@ func Knapsackv2(places []Place, timeLimit uint8, budget uint) (results []Place){
 	return optimalNode.Solution
 }
 
-
-func Score(places []Place) float64{
-	if len(places) == 1{
-		if places[0].Price == 0{
+func Score(places []Place) float64 {
+	if len(places) == 1 {
+		if places[0].Price == 0 {
 			return AvgRating / AvgPricing // set to average single place rating-price ratio
 		}
 		return float64(places[0].Rating) / places[0].Price
