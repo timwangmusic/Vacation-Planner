d9e9f3ce61d280400e7de164fff9374ceb7d04d3
Tim Wang <weihewang2012@gmail.com>
2020-07-29 11:26:25 -0700

    use gin for get planning API

--- go.mod
+++ go.mod
@@ -8,13 +8,19 @@ require (
 	github.com/braintree/manners v0.0.0-20160418043613-82a8879fc5fd
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/didip/tollbooth v4.0.2+incompatible
+	github.com/gin-gonic/gin v1.6.3
 	github.com/globalsign/mgo v0.0.0-20181015135952-eeefdecb41b8
+	github.com/go-playground/validator/v10 v10.3.0 // indirect
 	github.com/go-redis/redis/v7 v7.0.0-beta.4
+	github.com/golang/protobuf v1.4.2 // indirect
 	github.com/gomodule/redigo v2.0.0+incompatible // indirect
 	github.com/google/uuid v1.1.1 // indirect
 	github.com/gorilla/mux v1.7.3
+	github.com/json-iterator/go v1.1.10 // indirect
 	github.com/kelseyhightower/envconfig v1.4.0
 	github.com/konsorten/go-windows-terminal-sequences v1.0.2 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.1 // indirect
 	github.com/mpraski/clusters v0.0.0-20170921103932-51821d83008d
 	github.com/patrickmn/go-cache v2.1.0+incompatible // indirect
 	github.com/sergi/go-diff v1.0.0 // indirect
@@ -23,8 +29,10 @@ require (
 	github.com/yuin/gopher-lua v0.0.0-20190514113301-1cd887cd7036 // indirect
 	go.uber.org/zap v1.13.0
 	golang.org/x/crypto v0.0.0-20191219195013-becbf705a915
-	golang.org/x/sys v0.0.0-20190812172437-4e8604ab3aff // indirect
+	golang.org/x/sys v0.0.0-20200728102440-3e129f6d46b1 // indirect
 	golang.org/x/time v0.0.0-20190308202827-9d24e82272b4 // indirect
 	gonum.org/v1/gonum v0.0.0-20190808205415-ced62fe5104b
+	google.golang.org/protobuf v1.25.0 // indirect
 	googlemaps.github.io/maps v0.0.0-20190731233030-3b2ef8dcfc73
+	gopkg.in/yaml.v2 v2.3.0 // indirect
 )
--- planner/planner_API.go
+++ planner/planner_API.go
@@ -3,10 +3,7 @@ package planner
 import (
 	"encoding/json"
 	"errors"
-	rice "github.com/GeertJohan/go.rice"
-	"github.com/didip/tollbooth"
-	"github.com/didip/tollbooth/limiter"
-	"github.com/gorilla/mux"
+	"github.com/gin-gonic/gin"
 	"github.com/weihesdlegend/Vacation-planner/POI"
 	"github.com/weihesdlegend/Vacation-planner/iowrappers"
 	"github.com/weihesdlegend/Vacation-planner/solution"
@@ -212,43 +209,38 @@ func (planner *MyPlanner) postPlanningApi(w http.ResponseWriter, r *http.Request
 
 // HTTP GET API end-point
 // Return top planning result to user
-func (planner *MyPlanner) getPlanningApi(w http.ResponseWriter, r *http.Request) {
+func (planner *MyPlanner) getPlanningApi(c *gin.Context) {
 	var username = "guest" // default username
-	if planner.Environment == "production" {
+	if strings.ToLower(planner.Environment) == "production" {
 		var authenticationErr error
-		username, authenticationErr = planner.UserAuthentication(r)
+		username, authenticationErr = planner.UserAuthentication(c.Request)
 		if authenticationErr != nil {
-			w.WriteHeader(http.StatusUnauthorized)
-			_ = json.NewEncoder(w).Encode(authenticationErr.Error())
+			c.JSON(http.StatusUnauthorized, gin.H{ "error": authenticationErr.Error()})
 			return
 		}
 	}
 
-	vars := mux.Vars(r)
-	country := vars["country"]
-	city := vars["city"]
-	radius := vars["radius"]
-	weekday := vars["weekday"]
-	numResults := vars["numberResults"]
+	country := c.DefaultQuery("country", "USA")
+	city := c.DefaultQuery("city", "San Diego")
+	radius := c.DefaultQuery("radius", "10000")
+	weekday := c.DefaultQuery("weekday", "5") // Saturday
+	numResults := c.DefaultQuery("numberResults", "5")
 
 	numResultsInt, numResultsParsingErr := strconv.ParseUint(numResults, 10, 64)
 	if numResultsParsingErr != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		_, _ = w.Write([]byte("invalid number of planning results"))
+		c.String(http.StatusBadRequest, "number of planning results of %d is invalid", numResultsInt)
 		return
 	}
 	iowrappers.Logger.Debugf("number of requested planning results is %s", numResults)
 
 	weekdayUint, weekdayParsingErr := strconv.ParseUint(weekday, 10, 8)
 	if weekdayParsingErr != nil || weekdayUint < 0 || weekdayUint > 6 {
-		w.WriteHeader(http.StatusBadRequest)
-		_, _ = w.Write([]byte("invalid weekday"))
+		c.String(http.StatusBadRequest, "invalid weekday %d", weekdayUint)
 		return
 	}
 
 	if !validateSearchRadius(radius) {
-		w.WriteHeader(http.StatusBadRequest)
-		_, _ = w.Write([]byte("invalid search radius"))
+		c.String(http.StatusBadRequest, "invalid search radius of %s", radius)
 		return
 	}
 
@@ -267,43 +259,44 @@ func (planner *MyPlanner) getPlanningApi(w http.ResponseWriter, r *http.Request)
 	err := planningResp.Err
 	if err != "" {
 		if planningResp.StatusCode == solution.InvalidRequestLocation {
-			w.WriteHeader(http.StatusBadRequest)
-			_, _ = w.Write([]byte(err))
+			c.String(http.StatusBadRequest, err)
 		} else if planningResp.StatusCode == solution.NoValidSolution {
-			w.WriteHeader(http.StatusNotFound)
-			_, _ = w.Write([]byte("No valid solution is found.\n"))
-			_, _ = w.Write([]byte("Please try to search with larger radius."))
+			errString := "No valid solution is found.\n Please try to search with larger radius."
+			c.String(http.StatusBadRequest, errString)
 		}
 		return
 	}
 
-	utils.CheckErrImmediate(planner.ResultHTMLTemplate.Execute(w, planningResp), utils.LogError)
+	utils.CheckErrImmediate(planner.ResultHTMLTemplate.Execute(c.Writer, planningResp), utils.LogError)
 }
 
 func (planner MyPlanner) SetupRouter(serverPort string) *http.Server {
-	myRouter := mux.NewRouter()
+	myRouter := gin.Default()
+	//myRouter.GET("", planner.welcomeApi)
 
-	getLimiter := tollbooth.NewLimiter(MaxGetRequestsPerSecond, &limiter.ExpirableOptions{DefaultExpirationTTL: time.Second})
-	getLimiter.SetMethods([]string{"GET"})
-	getLimiter.SetMessage("You have reached maximum GET API limit")
+	v1 := myRouter.Group("/v1")
+	{
+		v1.GET("/plans", planner.getPlanningApi)
+		//v1.POST("", planner.postPlanningApi)
+	}
 
-	riceBox := rice.MustFindBox("../statics/scripts")
-	jsServingPath := "/statics/scripts"
-	jsFileServer := http.StripPrefix(jsServingPath, http.FileServer(riceBox.HTTPBox()))
-	myRouter.PathPrefix(jsServingPath).Handler(jsFileServer)
-	myRouter.HandleFunc("/", planner.welcomeApi)
-
-	myRouter.Path("/planning/v1").Queries("country", "{country}", "city", "{city}",
-		"radius", "{radius}", "weekday", "{weekday}", "numberResults", "{numberResults}").Handler(tollbooth.LimitFuncHandler(getLimiter, planner.getPlanningApi)).Methods("GET")
-
-	postLimiter := tollbooth.NewLimiter(MaxPostRequestsPerSecond, &limiter.ExpirableOptions{DefaultExpirationTTL: time.Second})
-	postLimiter.SetMethods([]string{"POST"})
-	postLimiter.SetMessage("You have reached maximum POST API limit")
-
-	myRouter.Handle("/planning/v1", tollbooth.LimitFuncHandler(postLimiter, planner.postPlanningApi)).Methods("POST")
-
-	myRouter.Path("/signup").HandlerFunc(planner.UserSignup).Methods("POST")
-	myRouter.Path("/login").HandlerFunc(planner.UserLogin).Methods("POST")
+	//riceBox := rice.MustFindBox("../statics/scripts")
+	//jsServingPath := "/statics/scripts"
+	//jsFileServer := http.StripPrefix(jsServingPath, http.FileServer(riceBox.HTTPBox()))
+	//myRouter.PathPrefix(jsServingPath).Handler(jsFileServer)
+	//myRouter.HandleFunc("/", planner.welcomeApi)
+	//
+	//myRouter.Path("/planning/v1").Queries("country", "{country}", "city", "{city}",
+	//	"radius", "{radius}", "weekday", "{weekday}", "numberResults", "{numberResults}").Handler(tollbooth.LimitFuncHandler(getLimiter, planner.getPlanningApi)).Methods("GET")
+	//
+	//postLimiter := tollbooth.NewLimiter(MaxPostRequestsPerSecond, &limiter.ExpirableOptions{DefaultExpirationTTL: time.Second})
+	//postLimiter.SetMethods([]string{"POST"})
+	//postLimiter.SetMessage("You have reached maximum POST API limit")
+	//
+	//myRouter.Handle("/planning/v1", tollbooth.LimitFuncHandler(postLimiter, planner.postPlanningApi)).Methods("POST")
+	//
+	//myRouter.Path("/signup").HandlerFunc(planner.UserSignup).Methods("POST")
+	//myRouter.Path("/login").HandlerFunc(planner.UserLogin).Methods("POST")
 
 	svr := &http.Server{
 		Addr:         ":" + serverPort,
