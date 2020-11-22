b83ee3133dab274a24234f04d03932e8a42bf7ec
Tim Wang <weihewang2012@gmail.com>
2020-11-22 13:38:01 -0800

    remove React code

--- planner-app/src/components/Header.js
+++
@@ -1,9 +0,0 @@
-import React , { Component } from "react"
-
-class Header extends Component {
-    render() {
-        return <header className="headerCommon">Vacation Plans for {this.props.TravelDestination}</header>
-    }
-}
-
-export default Header
