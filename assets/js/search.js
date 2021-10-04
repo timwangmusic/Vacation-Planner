f36b466a3e5915605fd8b0b0b6f0d5b047300d5b
Tim Wang <weihewang2012@gmail.com>
2021-10-03 23:06:47 -0700

    Fix: Set only JWT in session cookies

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,26 +1,28 @@
+// methods for the search page
+import jwt_decode from "./jwt-decode.js";
+
 function updateUsername() {
-    const username = Cookies.get('Username');
-    if (username) {
-        console.log(`username is ${username}`);
+    const jwt = Cookies.get("JWT");
+    let username = "guest";
+
+    if (jwt) {
+        console.log("The JWT token is: ", jwt);
+
+        const decodedJWT = jwt_decode(jwt);
+
+        username = decodedJWT.username;
+        console.log(`The current Logged-in username is ${decodedJWT.username}`)
     } else {
-        console.log("user is not logged in.");
+        console.log("The session has expired.");
+        return;
     }
 
-    const jwtToken = Cookies.get("JWT");
-    // if JWT is present then the session is still valid, otherwise JWT token will be removed
-    if (jwtToken) {
-        console.log(`Decoded JWT token ${jwtToken}`);
-    } else {
-        console.log("log in expired.");
-    }
+    document.getElementById("login").style.display = "none";
+    document.getElementById("signup").style.display = "none";
 
-    if (username && jwtToken) {
-        document.getElementById("login").style.display = "none";
-        document.getElementById("signup").style.display = "none";
+    const userNameElement = document.getElementById("username");
+    userNameElement.innerText = username;
 
-        const userNameElement = document.getElementById("username");
-        userNameElement.innerText = username;
-    }
 }
 
 updateUsername();
@@ -48,8 +50,8 @@ function locateMe() {
                     if (month < 10) {
                         month = "0" + month.toString();
                     }
-                    let day = today.getDate(); 
-                    if (day < 10) { 
+                    let day = today.getDate();
+                    if (day < 10) {
                         day = "0" + day.toString();
                     }
                     document.getElementById("datepicker").value = [today.getFullYear(), month, day].join("-");
@@ -73,47 +75,9 @@ const locationSearchInput = document.getElementById('location');
 const spinner = document.getElementById("searchSpinner");
 locationSearchInput.addEventListener(
     "keyup", (evt) => {
-        if (evt.key == "Enter") {
+        if (evt.key === "Enter") {
             console.log("Pressed Enter in location input!")
             spinner.classList.remove("visually-hidden");
-        }    
+        }
     }
 )
-
-const cities = [
-    "San Jose",
-    "San Diego",
-    "San Francisco",
-    "Los Angeles",
-    "New York",
-    "Chicago",
-    "Houston",
-    "Philadelphia",
-    "Phoenix",
-    "San Antonio",
-    "Dallas",
-    "Indianapolis",
-    "Austin",
-    "Columbus",
-    "Baltimore",
-    "Boston",
-    "Seattle",
-    "Washington",
-    "Portland",
-    "Las Vegas",
-    "Paris",
-    "Rome",
-    "Vancouver",
-    "New Delhi",
-    "Beijing",
-    "Shanghai",
-];
-
-const countries = [
-    "USA",
-    "Italy",
-    "France",
-    "Canada",
-    "China",
-    "India",
-]
