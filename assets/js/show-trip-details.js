a5e03553645a0227d108e0eaa205de0c6be54cd1
Rui Wang <rwangsc18@gmail.com>
2022-02-23 11:29:20 -0800

    Add embedded Google map with place locations in trip show page
    -- main JS logics are in google_map.js
    -- use Google map javascript API
    -- add place lat/lng into server resp of trip show page

--- assets/js/show-trip-details.js
+++ assets/js/show-trip-details.js
@@ -1,19 +1,10 @@
 import { updateUsername } from "./user.js";
 import { Place, View } from "./place.js";
 
-const cardBodies = document.querySelectorAll("div.card div.card-body");
-const myCarousel = document.querySelector('#carouselExampleIndicators');
-const saveIcon = document.querySelector("i.bi-bookmark"); 
+const saveIcon = document.querySelector("i.bi-bookmark");
 const saveTooltip = new bootstrap.Tooltip(saveIcon);
 const profile = document.getElementById("profile");
-
-myCarousel.addEventListener('slide.bs.carousel', function (evt) {
-    // show new card body and hide the old one
-    cardBodies[evt.from].classList.add("d-none");
-    cardBodies[evt.to].classList.remove("d-none");
-})
-
-const username = updateUsername(); 
+const username = updateUsername();
 
 // save bookmark tooltip
 saveTooltip.disable();
@@ -21,7 +12,7 @@ saveIcon.addEventListener('click', postPlanForUser)
 
 async function postPlanForUser() {
     const url = `/v1/users/${username}/plans`
-    const plan = await getPlan();
+    const plan = await getTravelPlan();
 
     try {
         console.log("Sending post request to", url)
@@ -32,11 +23,11 @@ async function postPlanForUser() {
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify(planToView(plan))
-            }
+        }
         )
     } catch (e) {
-        console.log("Err when saving plan:", e) 
-        return 
+        console.log("Err when saving plan:", e)
+        return
     }
 
     this.classList.remove("bi-bookmark")
@@ -47,68 +38,42 @@ async function postPlanForUser() {
     saveTooltip.show();
 }
 
-async function getPlan() {
-    const planUrl = toPlanJSONURL(document.URL)
-
-    return await fetch(
-        planUrl, {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-    }
-    )
-        .then(response =>
-            response.json()
-        )
-        .catch(
-            err => console.log(err)
-        )
-}
-
 function planToView(plan) {
     const url = new URL(document.URL);
     const planId = getPlanId(url);
     const travelDate = getTravelDate(url);
-    const view = new View (
+    const view = new View(
         plan.TravelDestination,
         travelDate, // YYYY-MM-DD
-        planId, 
+        planId,
         new Date().toISOString(),
         []
     )
     for (const pDetail of plan.PlaceDetails) {
         const {
-            Name: placeName, 
-            FormattedAdress: address, 
+            Name: placeName,
+            FormattedAdress: address,
             TimePeriod: timePeriod = '10 - 16', // TODO: use actual time period for each place
             URL: mapURL
-        } = pDetail 
+        } = pDetail
         const place = new Place(timePeriod, placeName, address, mapURL)
         view.places.push(place)
     }
-    return view 
+    return view
 }
 
 function getPlanId(url) {
     const results = url.pathname.split('/')
-    return results[results.length-1]
+    return results[results.length - 1]
 }
 
-function getTravelDate(url){
+function getTravelDate(url) {
     const searchParams = new URLSearchParams(url.search);
     const date = searchParams.get("date");
     if (date) {
-        return date 
+        return date
     }
     return new Date().toISOString().split('T')[0] // YYYY-MM-DD, current date
-}
-function toPlanJSONURL(pageURL) {
-    const url = new URL(pageURL)
-    const searchParams = new URLSearchParams(url.search);
-    searchParams.append('json_only', 'true')
-    url.search = searchParams.toString() 
-    return url.toString(); 
 }
 
 // Link to User Profile page
