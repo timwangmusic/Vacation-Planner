4b135477276165fcbceb4e9aa20c3b61de893751
Sandeep <sandeep.maxcraze@gmail.com>
2022-04-30 22:44:10 -0700

    add search.js and check changes
    add plan template now
    
    add changes for search_page.html
    
    add changes to search results layout template
    
    add newline
    
    removed console logging for now

--- assets/js/search.js
+++ assets/js/search.js
@@ -6,26 +6,28 @@ import { logOut } from "./user.js";
 setDateToday();
 
 (function ($) {
-  $("#location").autocomplete({
-    source: function (request, response) {
-      $.ajax({
-        url: "/v1/cities",
-        dataType: "json",
-        data: { term: request.term },
-        success: function (data) {
-          response(
-            $.map(data.results, function (city) {
-              if (city.region) {
-                return [city.city, city.region, city.country].join(", ");
-              }
-              return [city.city, city.country].join(", ");
-            })
-          );
-        },
-      });
-    },
-    minLength: 2,
-  });
+    $("#location").autocomplete(
+        {
+            source: function (request, response) {
+                $.ajax(
+                    {
+                        url: "/v1/cities",
+                        dataType: "json",
+                        data: { term: request.term },
+                        success: function (data) {
+                            response($.map(data.results, function (city) {
+                                if (city.region) {
+                                    return [city.city, city.region, city.country].join(", ")
+                                }
+                                return [city.city, city.country].join(", ")
+                            }))
+                        }
+                    }
+                )
+            },
+            minLength: 2,
+        }
+    )
 })(jQuery);
 
 $(document).ready(function CheckPreviousLocation() {
@@ -39,29 +41,24 @@ $(document).ready(function CheckPreviousLocation() {
   }
 });
 
-document.getElementById("logout-confirm-btn").addEventListener("click", logOut);
+document.querySelector('#autofill').addEventListener('click', locateMe);
 
-document
-  .querySelector("#use-precise-location")
-  .addEventListener("click", preciseLocation);
+document.querySelector('#use-precise-location').addEventListener('click', preciseLocation);
 
-const locationSearchInput = document.getElementById("location");
+const locationSearchInput = document.getElementById('location');
 const spinner = document.getElementById("searchSpinner");
-locationSearchInput.addEventListener("keyup", (evt) => {
-  if (evt.key === "Enter") {
-    console.log("Pressed Enter in location input!");
-    spinner.classList.remove("visually-hidden");
-  }
-});
+locationSearchInput.addEventListener(
+    "keyup", (evt) => {
+        if (evt.key === "Enter") {
+            console.log("Pressed Enter in location input!")
+            spinner.classList.remove("visually-hidden");
+        }
+    }
+);
 // hide spinner when switching pages
 const hideSpinner = function () {
-  spinner.classList.add("visually-hidden");
-};
+    spinner.classList.add("visually-hidden");
+}
 document.addEventListener("visibilitychange", hideSpinner);
 
-document
-  .getElementById("profile")
-  .addEventListener(
-    "click",
-    () => (window.location = `/v1/users/${username}/profile`)
-  );
+document.getElementById("profile").addEventListener("click", () => window.location = `/v1/users/${username}/profile`);
