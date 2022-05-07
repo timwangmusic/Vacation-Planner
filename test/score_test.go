bfa1761ca62d94801497b6e596ec015171e5166b
Rui Wang <rwangsc18@gmail.com>
2022-05-07 16:09:19 -0700

    Add unit tests for updated plan score method
    - TODO: add more tests for distance penalty

--- test/score_test.go
+++ test/score_test.go
@@ -3,37 +3,65 @@ package test
 import (
 	"testing"
 
+	"github.com/stretchr/testify/assert"
 	"github.com/weihesdlegend/Vacation-planner/POI"
 	"github.com/weihesdlegend/Vacation-planner/matching"
-	"github.com/weihesdlegend/Vacation-planner/solution"
 )
 
-func TestScoreFunction(t *testing.T) {
-	// test regular non-zero-price place
-	place1 := matching.CreatePlace(POI.Place{
+var testSinglePlaces = []matching.Place{
+	matching.CreatePlace(POI.Place{
 		PriceLevel:       POI.PriceLevelThree,
 		Rating:           5.0,
 		UserRatingsTotal: 99,
-	}, POI.PlaceCategoryVisit)
+	}, POI.PlaceCategoryVisit),
+	matching.CreatePlace(POI.Place{
+		PriceLevel:       POI.PriceLevelZero,
+		Rating:           3.0,
+		UserRatingsTotal: 0,
+	}, POI.PlaceCategoryVisit),
+}
 
+func TestLegacyScoreSinglePlacePlan(t *testing.T) {
+	// test regular non-zero-price place
 	expectedScore := 0.2
-	score := matching.Score([]matching.Place{place1}, solution.DefaultPlaceSearchRadius)
+	score := matching.ScoreOld([]matching.Place{testSinglePlaces[0]})
 	if score != expectedScore {
 		t.Errorf("Expected score %f, got %f", expectedScore, score)
 		return
 	}
 
 	// test zero-price place
-	place2 := matching.CreatePlace(POI.Place{
-		PriceLevel:       POI.PriceLevelZero,
-		Rating:           3.0,
-		UserRatingsTotal: 0,
-	}, POI.PlaceCategoryVisit)
-
 	expectedScore = 0.0
-	score = matching.Score([]matching.Place{place2}, solution.DefaultPlaceSearchRadius)
+	score = matching.ScoreOld([]matching.Place{testSinglePlaces[1]})
 	if score != expectedScore {
 		t.Errorf("Expected score %f, got %f", expectedScore, score)
 		return
+	}
+}
+
+func TestScoreSinglePlacePlan(t *testing.T) {
+	// place1 := matching.CreatePlace(POI.Place{
+	// 	PriceLevel:       POI.PriceLevelThree,
+	// 	Rating:           5.0,
+	// 	UserRatingsTotal: 99,
+	// }, POI.PlaceCategoryVisit)
+	// place2 := matching.CreatePlace(POI.Place{
+	// 	PriceLevel:       POI.PriceLevelZero,
+	// 	Rating:           3.0,
+	// 	UserRatingsTotal: 0,
+	// }, POI.PlaceCategoryVisit)
+
+	tests := []struct {
+		place        []matching.Place
+		expectScore  float64
+		searchRadius int
+	}{
+		{[]matching.Place{testSinglePlaces[0]}, 0.2, 10000},
+		{[]matching.Place{testSinglePlaces[1]}, 0.0, 1000},
+	}
+
+	for _, test := range tests {
+		score := matching.Score(test.place, test.searchRadius)
+		assert.Equal(t, score, test.expectScore)
 	}
 }
