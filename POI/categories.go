9989afd52d920bd1c703731448b8a68286a2b56f
Tim Wang <weihewang2012@gmail.com>
2023-02-16 12:06:20 -0800

    Chore: combine files in POI package

--- POI/categories.go
+++ POI/categories.go
@@ -38,7 +38,7 @@ func GetPlaceCategory(placeType LocationType) (placeCategory PlaceCategory) {
 	return
 }
 
-// Given a location type returns a set of types defined in google maps API
+// GetPlaceTypes returns a set of types defined in Google Maps API given a location type
 func GetPlaceTypes(placeCat PlaceCategory) (placeTypes []LocationType) {
 	switch placeCat {
 	case PlaceCategoryVisit:
@@ -49,4 +49,28 @@ func GetPlaceTypes(placeCat PlaceCategory) (placeTypes []LocationType) {
 			[]LocationType{LocationTypeCafe, LocationTypeRestaurant}...)
 	}
 	return
+}
+
+type StayingTime uint8
+
+const (
+	StayingTimeLocationTypeCafe          = StayingTime(1)
+	StayingTimeLocationTypeRestaurant    = StayingTime(1)
+	StayingTimeLocationTypeMuseum        = StayingTime(3)
+	StayingTimeLocationTypeGallery       = StayingTime(2)
+	StayingTimeLocationTypeAmusementPark = StayingTime(3)
+	StayingTimeLocationTypePark          = StayingTime(2)
+)
+
+func GetStayingTimeForLocationType(locationType LocationType) StayingTime {
+	var stayingTimeMap = map[LocationType]StayingTime{
+		LocationTypeCafe:          StayingTimeLocationTypeCafe,
+		LocationTypeRestaurant:    StayingTimeLocationTypeRestaurant,
+		LocationTypeMuseum:        StayingTimeLocationTypeMuseum,
+		LocationTypeGallery:       StayingTimeLocationTypeGallery,
+		LocationTypeAmusementPark: StayingTimeLocationTypeAmusementPark,
+		LocationTypePark:          StayingTimeLocationTypePark,
+	}
+
+	return stayingTimeMap[locationType]
 }
--- POI/places.go
+++ POI/places.go
@@ -83,6 +83,10 @@ type Address struct {
 
 type PriceLevel int
 
+type OpeningHours struct {
+	Hours []string
+}
+
 const (
 	PriceLevelZero    = 0
 	PriceLevelOne     = 1
@@ -262,4 +266,33 @@ func (place *Place) SetPhoto(photo *maps.Photo) {
 
 func (place *Place) SetUserRatingsTotal(userRatingsTotal int) {
 	place.UserRatingsTotal = userRatingsTotal
+}
+
+func CreatePlace(name, addr, formattedAddr, businessStatus string, locationType LocationType, openingHours *OpeningHours, placeID string, priceLevel int, rating float32, url string, photo *maps.Photo, userRatingsTotal int, latitude, longitude float64) (place Place) {
+	place.SetType(locationType)
+	place.SetName(name)
+	place.SetID(placeID)
+	var weekday Weekday
+	if openingHours != nil && openingHours.Hours != nil {
+		for weekday = DateMonday; weekday <= DateSunday; weekday++ {
+			place.SetHour(weekday, openingHours.Hours[weekday])
+		}
+	}
+	// set default
+	for weekday = DateMonday; weekday <= DateSunday; weekday++ {
+		if place.GetHour(weekday) == "" {
+			place.SetHour(weekday, "8:30 am – 9:30 pm")
+		}
+	}
+
+	place.SetStatus(businessStatus)
+	place.SetLocationCoordinates([2]float64{latitude, longitude})
+	place.SetAddress(addr)
+	place.SetFormattedAddress(formattedAddr)
+	place.SetPriceLevel(priceLevel)
+	place.SetRating(rating)
+	place.SetURL(url)
+	place.SetPhoto(photo)
+	place.SetUserRatingsTotal(userRatingsTotal)
+	return
 }
