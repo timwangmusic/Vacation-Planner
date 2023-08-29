6a445586dd6dd74dcd3bd76428c2b7cd44f7ca09
Tim Wang <weihewang2012@gmail.com>
2023-08-29 14:21:34 -0700

    Enhancement: execute user favorites and saved plan functions concurrently

--- assets/js/user_profile.js
+++ assets/js/user_profile.js
@@ -1,118 +1,113 @@
-import { updateUsername } from './user.js';
+import { updateUsername } from "./user.js";
 
 const username = updateUsername();
 
 async function deleteUserPlan() {
-    const username = this.dataset.user;
-    const planId = this.dataset.planid;
-    const url = `/v1/users/${username}/plan/${planId}`;
+  const username = this.dataset.user;
+  const planId = this.dataset.planid;
+  const url = `/v1/users/${username}/plan/${planId}`;
 
-    await axios.delete(
-        url
-    ).then(
-        response => {
-            console.log(response.status)
-            location.reload();
-        }
-    ).catch(
-        err => console.error(err)
-    );
+  await axios
+    .delete(url)
+    .then((response) => {
+      console.log(response.status);
+      location.reload();
+    })
+    .catch((err) => console.error(err));
 }
 
 function renderCard(plan) {
-    const cards = $('#cards');
-    let card = $('<div>').addClass('card rounded mb-2').css('max-width', '350px');
-    let cardBody = $('<div>').addClass('card-body');
-    cardBody.append($('<h5>').addClass('card-title').text(plan.destination));
-    cardBody.append($('<h6>').addClass('card-subtitle').text(plan.travel_date));
-    let placeList = $('<ul>').addClass('list-group list-group-flush');
-    plan.places.forEach(
-        place => {
-            let p = $('<li>').addClass('list-group-item');
-            p.append($('<a>').addClass('card-link').attr('href', place.url).text(place.place_name));
-            placeList.append(p);
-        }
-    )
-    cardBody.append(placeList);
-    let deleteButton = $('<button>')
-        .addClass('btn btn-outline-warning m-1 float-end')
-        .attr('type', 'button')
-        .attr('data-planid', `${plan.id}`)
-        .attr('data-user', `${username}`)
-        .text('delete');
-    deleteButton.click(deleteUserPlan);
-    cardBody.append(deleteButton);
+  const cards = $("#cards");
+  let card = $("<div>").addClass("card rounded mb-2").css("max-width", "350px");
+  let cardBody = $("<div>").addClass("card-body");
+  cardBody.append($("<h5>").addClass("card-title").text(plan.destination));
+  cardBody.append($("<h6>").addClass("card-subtitle").text(plan.travel_date));
+  let placeList = $("<ul>").addClass("list-group list-group-flush");
+  plan.places.forEach((place) => {
+    let p = $("<li>").addClass("list-group-item");
+    p.append(
+      $("<a>")
+        .addClass("card-link")
+        .attr("href", place.url)
+        .text(place.place_name)
+    );
+    placeList.append(p);
+  });
+  cardBody.append(placeList);
+  let deleteButton = $("<button>")
+    .addClass("btn btn-outline-warning m-1 float-end")
+    .attr("type", "button")
+    .attr("data-planid", `${plan.id}`)
+    .attr("data-user", `${username}`)
+    .text("delete");
+  deleteButton.click(deleteUserPlan);
+  cardBody.append(deleteButton);
 
-    let showDetailsButton = $('<button>')
-        .addClass('btn btn-outline-info float-end m-1')
-        .attr('type', 'button')
-        .attr('data-planid', `${plan.original_plan_id}`)
-        .text('details');
-    showDetailsButton.click(showPlanDetails);
-    cardBody.append(showDetailsButton);
+  let showDetailsButton = $("<button>")
+    .addClass("btn btn-outline-info float-end m-1")
+    .attr("type", "button")
+    .attr("data-planid", `${plan.original_plan_id}`)
+    .text("details");
+  showDetailsButton.click(showPlanDetails);
+  cardBody.append(showDetailsButton);
 
-    card.append(cardBody);
-    cards.append(card);
+  card.append(cardBody);
+  cards.append(card);
 }
 
 function renderFavorites(favorites) {
-    document.getElementById("most-searched-place").style.maxWidth = "350px";
-    const mostSearchedPlace = document.querySelector('#most-searched-place .card-body .card-text');
-    let result = '';
-    let count = 0;
-    for (const location in favorites) {
-        if (favorites[location].count > count) {
-            result = favorites[location].location;
-            count = favorites[location].count;
-        }
+  document.getElementById("most-searched-place").style.maxWidth = "350px";
+  const mostSearchedPlace = document.querySelector(
+    "#most-searched-place .card-body .card-text"
+  );
+  let result = "";
+  let count = 0;
+  for (const location in favorites) {
+    if (favorites[location].count > count) {
+      result = favorites[location].location;
+      count = favorites[location].count;
     }
-    mostSearchedPlace.innerText = result;
+  }
+  mostSearchedPlace.innerText = result;
 }
 
 function showPlanDetails() {
-    const planID = this.dataset.planid;
-    window.location = `plans/${planID}`;
+  const planID = this.dataset.planid;
+  window.location = `plans/${planID}`;
 }
 
-async function getUserPlans() {
-    const url = `/v1/users/${username}/plans`;
-    await axios.get(
-        url
-    ).then(
-        response => {
-            const data = response.data;
-            const plans = data['travel_plans']
-            if (plans.length > 0) {
-                for (let i = 0; i < plans.length; i++) {
-                    renderCard(plans[i]);
-                }
-            }
+function getUserPlans() {
+  const url = `/v1/users/${username}/plans`;
+  return axios
+    .get(url)
+    .then((response) => {
+      const data = response.data;
+      const plans = data["travel_plans"];
+      if (plans.length > 0) {
+        for (let i = 0; i < plans.length; i++) {
+          renderCard(plans[i]);
         }
-    ).catch(
-        err => console.error(err)
-    );
+      }
+    })
+    .catch((err) => console.error(err));
 }
 
-async function getUserFavorites() {
-    const favoritesUrl = `/v1/users/${username}/favorites`;
-    await axios.get(
-        favoritesUrl
-    ).then(
-        response => {
-            const data = response.data;
-            const favorites = data['searchHistory'];
-            if (Object.keys(favorites).length > 0) {
-                renderFavorites(favorites);
-            }
-        }
-    ).catch(
-        err => console.error(err)
-    );
+function getUserFavorites() {
+  const favoritesUrl = `/v1/users/${username}/favorites`;
+  return axios
+    .get(favoritesUrl)
+    .then((response) => {
+      const data = response.data;
+      const favorites = data["searchHistory"];
+      if (Object.keys(favorites).length > 0) {
+        renderFavorites(favorites);
+      }
+    })
+    .catch((err) => console.error(err));
 }
 
 async function renderUserProfile() {
-    await getUserPlans();
-    await getUserFavorites();
+  await Promise.all([getUserPlans(), getUserFavorites()]);
 }
 
-window.onload = renderUserProfile();
+renderUserProfile().then(() => console.log("user profile is loaded"));
