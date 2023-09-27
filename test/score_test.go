841736d0f48600c8fe3d65977f787f0d776e460a
Tim Wang <weihewang2012@gmail.com>
2023-09-27 11:01:44 -0700

    Improve tests for the score function

--- test/score_test.go
+++ test/score_test.go
@@ -1,6 +1,7 @@
 package test
 
 import (
+	"math"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -13,55 +14,26 @@ var testSinglePlaces = []matching.Place{
 		PriceLevel:       POI.PriceLevelThree,
 		Rating:           5.0,
 		UserRatingsTotal: 99,
-	}, POI.PlaceCategoryVisit),
+	}, POI.PlaceCategoryEatery),
 	matching.CreatePlace(POI.Place{
 		PriceLevel:       POI.PriceLevelZero,
 		Rating:           3.0,
-		UserRatingsTotal: 0,
+		UserRatingsTotal: 999,
 	}, POI.PlaceCategoryVisit),
 }
 
-func TestLegacyScoreSinglePlacePlan(t *testing.T) {
-	// test regular non-zero-price place
-	expectedScore := 0.2
-	score := matching.ScoreOld([]matching.Place{testSinglePlaces[0]})
-	if score != expectedScore {
-		t.Errorf("Expected score %f, got %f", expectedScore, score)
-		return
-	}
-
-	// test zero-price place
-	expectedScore = 0.0
-	score = matching.ScoreOld([]matching.Place{testSinglePlaces[1]})
-	if score != expectedScore {
-		t.Errorf("Expected score %f, got %f", expectedScore, score)
-		return
-	}
-}
-
 func TestScoreSinglePlacePlan(t *testing.T) {
-	// place1 := matching.CreatePlace(POI.Place{
-	// 	PriceLevel:       POI.PriceLevelThree,
-	// 	Rating:           5.0,
-	// 	UserRatingsTotal: 99,
-	// }, POI.PlaceCategoryVisit)
-	// place2 := matching.CreatePlace(POI.Place{
-	// 	PriceLevel:       POI.PriceLevelZero,
-	// 	Rating:           3.0,
-	// 	UserRatingsTotal: 0,
-	// }, POI.PlaceCategoryVisit)
-
 	tests := []struct {
 		place        []matching.Place
 		expectScore  float64
 		searchRadius int
 	}{
 		{[]matching.Place{testSinglePlaces[0]}, 0.2, 10000},
-		{[]matching.Place{testSinglePlaces[1]}, 0.0, 1000},
+		{[]matching.Place{testSinglePlaces[1]}, 1.8, 10000},
 	}
 
 	for _, test := range tests {
 		score := matching.Score(test.place, test.searchRadius)
-		assert.Equal(t, score, test.expectScore)
+		assert.Equal(t, test.expectScore, math.Ceil(score*100)/100)
 	}
 }
