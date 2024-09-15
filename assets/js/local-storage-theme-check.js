6e0d4af1fbca59249030f41303402bbf9b5e520f
Sandeep Anand <sandeep.maxcraze@gmail.com>
2024-09-15 07:48:30 -0700

    delete local-storage-theme-check.js, its not needed anymore

--- assets/js/local-storage-theme-check.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", checkTheme);
-
-function checkTheme() {
-  // Check local Storage here for theme
-  let localStore = localStorage.getItem("theme");
-  if (localStore === "dark") {
-    document.documentElement.setAttribute("data-theme", "dark");
-  }
-}
