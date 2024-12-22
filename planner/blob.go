4421e1cb08a98830e302ed9ee22e08f3ae13a49a
Tim Wang <weihewang2012@gmail.com>
2024-12-21 23:56:58 -0800

    fix: gen AI image endpoint returns photo URL after uploading to blob storage

--- planner/blob.go
+++ planner/blob.go
@@ -38,17 +38,18 @@ func (p *MyPlanner) getLocationImage(ctx *gin.Context) {
 		iowrappers.Logger.Debugf("failed to get photos for location %+v: %v", location, err)
 	}
 
-	testBucket := p.BlobBucket
+	blobBucket := p.BlobBucket
 
 	if err == nil && len(photos) > 0 {
 		r := rand.New(rand.NewSource(time.Now().UnixNano()))
 		selected := photos[r.Intn(len(photos))]
+		blobKey := strings.Join([]string{locationToBlobKey(location), selected}, "/")
 		url, err := c.PresignedURL(ctx, &awsinternal.BlobMetaData{
-			Bucket: testBucket,
-			Key:    strings.Join([]string{locationToBlobKey(location), selected}, "/"),
+			Bucket: blobBucket,
+			Key:    blobKey,
 		})
 		if err != nil {
-			iowrappers.Logger.Debugf("failed to get location photos: %v", err)
+			iowrappers.Logger.Debugf("failed to get location photos with key %s: %v", blobKey, err)
 		} else {
 			ctx.JSON(http.StatusOK, gin.H{"photo": url.String()})
 			return
@@ -64,16 +65,28 @@ func (p *MyPlanner) getLocationImage(ctx *gin.Context) {
 	}
 
 	photoId := uuid.New().String() + ".png"
+	blobKey := strings.Join([]string{location.Country, location.AdminAreaLevelOne,
+		location.City, photoId}, "/")
 
 	if err = c.Upload(ctx, &awsinternal.BlobMetaData{
-		Bucket: testBucket,
-		Key: strings.Join([]string{location.Country, location.AdminAreaLevelOne,
-			location.City, photoId}, "/"),
+		Bucket: blobBucket,
+		Key:    blobKey,
 	}, imgBytes); err != nil {
+		ctx.JSON(http.StatusInternalServerError, err)
+	}
+
+	url, err := c.PresignedURL(ctx, &awsinternal.BlobMetaData{
+		Bucket: blobBucket,
+		Key:    blobKey,
+	})
+
+	if err != nil {
 		ctx.JSON(http.StatusInternalServerError, err)
 	}
 
 	if err = p.RedisClient.SaveLocationPhotoID(ctx, location, photoId); err != nil {
 		iowrappers.Logger.Debugf("failed to save location photo: %v", err)
 	}
+
+	ctx.JSON(http.StatusOK, gin.H{"photo": url.String()})
 }
