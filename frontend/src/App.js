51d0937d366f4a6e69cd18d184468a21590adb7c
Sandeep Anand <sandeep.maxcraze@gmail.com>
2025-07-05 07:51:12 -0700

    darkMode with bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,20 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import { ThemeProvider } from './contexts/ThemeContext';
 import { Title } from "./title";
 import "./App.css";
 import { RequestForm } from "./requestForm";
 import DarkModeToggle from "./DarkModeToggle";
 
 function App() {
-  return (
-    <>
-      <div className="min-h-screen bg-white dark:bg-gray-900 text-black dark:text-white p-6">
-        <DarkModeToggle />
-          <div className="min-h-screen bg-white dark:bg-gray-900 text-black dark:text-white p-6">
-              <Title />
-              <RequestForm />
-          </div>
-      </div>
-    </>
-  );
+    return (
+        <ThemeProvider>
+            <div className="min-vh-100 p-3">
+                <DarkModeToggle />
+                <Title />
+                <RequestForm />
+            </div>
+        </ThemeProvider>
+    );
 }
 
 export default App;
--- frontend/src/title.js
+++ frontend/src/title.js
@@ -1,9 +1,15 @@
+import { useTheme } from './contexts/ThemeContext';
+
 export function Title() {
+  const { isDarkMode } = useTheme();
+
   return (
-    <h1
-        className="text-[#24c1e0] dark:text-[#1aa1bd] text-center bg-whitesmoke dark:bg-darksmoke leading-[100px] m-0"
-    >
-      Vacation Planner
-    </h1>
+      <div className={`p-4 ${isDarkMode ? 'bg-darksmoke' : 'bg-white'}`}>
+          <div className="flex items-center justify-center bg-whitesmoke dark:bg-darksmoke">
+              <h1 className="text-6xl md:text-7xl font-bold text-[#24c1e0] dark:text-[#1aa1bd] text-center">
+                  Vacation Planner
+              </h1>
+          </div>
+      </div>
   );
 }
